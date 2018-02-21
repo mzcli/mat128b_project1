edffc0b5662f29e2ecebeddc308e512779468c7f
mzcli <36706855+mzcli@users.noreply.github.com>
2018-02-21 10:52:06 -0800

    Update part4.m

--- part4.m
+++ part4.m
@@ -1,6 +1,22 @@
-% part 4 boxcount
+% part 4 two methods
 function part4(imag)
     c = imread(imag);
-    [n, r] = boxcount(c, 'slope')
-    loglog(r, n,'bo-', r, (r/r(end)).^(-2), 'r--')
+    c = rgb2gray(c);
+    [m, n] = size(c);
+    l = [1 : 100];
+    kount = zeros([1,100]);
+    for k = 1 : 100
+        for i = 1 : k : m
+            for j = 1 : k : n
+                if c(i, j) ~= 255
+                    kount(k) = kount(k) + 1;
+                end;
+            end
+        end
+    end   
+    hold on;
+    [n, r] = boxcount(imag);
+    loglog(r, n, '--r')
+    loglog(l, kount, '--s');
+    hold off;
 end
